4c6a61143750dd2c09af4c9e531cd100ff7c2f9d
thecedet <thecedet@live.fr>
2023-04-13 18:49:29 +0200

    remove cart page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,7 +22,7 @@ export default function NavBar({cart, setHash}) {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                        <li className="nav-item"><Link className="nav-link" to="https://github.com">GitHub</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to="https://github.com/thecedet/AmazonLike-WEB">GitHub</Link></li>
                         <li className="nav-item dropdown">
                             {window.location.pathname === "/" && (<a className="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Catégories</a>)}
                             <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
