2e695a9c45a068c6a080c3802e62cc26c0a44b57
thecedet <thecedet@live.fr>
2023-04-13 18:51:21 +0200

    it's a joke bro

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     return (
 
         <footer className="py-5 bg-dark">
-            <div className="container"><p className="m-0 text-center text-white">Copyright &copy; Your Website 2023</p></div>
+            <div className="container"><p className="m-0 text-center text-white">Copyright &copy; Your Website 2023 &lt;-- C'est une blague</p></div>
         </footer>
 
     );
